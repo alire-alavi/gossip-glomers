29fcce3b2b421ee9de6306ff8a42619b9a0de748
alire-alavi <alirezaalavirazavi@hotmail.com>
2025-11-01 17:01:03 +0330

    better multi message type handling

--- src/bin/echo.rs
+++ src/bin/echo.rs
@@ -1,5 +1,6 @@
-use anyhow::{bail, Context};
 use gossip_glomers::*;
+
+use anyhow::Context;
 use serde::{Deserialize, Serialize};
 use std::io::{StdoutLock, Write};
 
@@ -7,24 +8,21 @@ use std::io::{StdoutLock, Write};
 #[serde(tag = "type")]
 #[serde(rename_all = "snake_case")]
 enum Payload {
-    Echo {
-        echo: String,
-    },
-    EchoOk {
-        echo: String,
-    },
-    Init {
-        node_id: String,
-        node_ids: Vec<String>,
-    },
-    InitOk,
+    Echo { echo: String },
+    EchoOk { echo: String },
 }
 
 struct EchoNode {
     id: usize,
 }
 
-impl Node<Payload> for EchoNode {
+impl Node<(), Payload> for EchoNode {
+    fn from_init(_state: (), _init: gossip_glomers::Init) -> anyhow::Result<Self>
+    where
+        Self: Sized,
+    {
+        Ok(EchoNode { id: 1 })
+    }
     fn step(&mut self, input: Message<Payload>, output: &mut StdoutLock) -> anyhow::Result<()> {
         match input.body.payload {
             Payload::Echo { echo } => {
@@ -46,31 +44,11 @@ impl Node<Payload> for EchoNode {
                 self.id += 1;
             }
             Payload::EchoOk { .. } => {}
-            Payload::Init { .. } => {
-                let reply = Message {
-                    src: input.dst,
-                    dst: input.src,
-                    body: Body {
-                        id: Some(self.id),
-                        in_reply_to: input.body.id,
-                        payload: Payload::InitOk,
-                    },
-                };
-                serde_json::to_writer(&mut *output, &reply)
-                    .context("serialize response to init")?;
-                output.write_all(b"\n").context("write trailing newline")?;
-                // reply
-                //     .serialize(output)
-                //     .context("Serialize response to init")
-                //     .unwrap();
-                self.id += 1;
-            }
-            Payload::InitOk { .. } => bail!("received init_ok message"),
         }
         Ok(())
     }
 }
 
 fn main() -> anyhow::Result<()> {
-    main_loop(EchoNode { id: 0 })
+    main_loop::<_, EchoNode, _>(())
 }
--- src/bin/unique-ids.rs
+++ src/bin/unique-ids.rs
@@ -1,33 +1,35 @@
-use anyhow::{bail, Context};
+use anyhow::Context;
 use gossip_glomers::*;
 use serde::{Deserialize, Serialize};
 use std::io::{StdoutLock, Write};
-use ulid::Ulid;
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
 #[serde(tag = "type")]
 #[serde(rename_all = "snake_case")]
 enum Payload {
     Generate,
-    GenerateOk {
-        id: String,
-    },
-    Init {
-        node_id: String,
-        node_ids: Vec<String>,
-    },
-    InitOk,
+    GenerateOk { id: String },
 }
 
 struct UniqueNode {
+    node: String,
     id: usize,
 }
 
-impl Node<Payload> for UniqueNode {
+impl Node<(), Payload> for UniqueNode {
+    fn from_init(_state: (), init: gossip_glomers::Init) -> anyhow::Result<Self>
+    where
+        Self: Sized,
+    {
+        Ok(UniqueNode {
+            id: 1,
+            node: init.node_id,
+        })
+    }
     fn step(&mut self, input: Message<Payload>, output: &mut StdoutLock) -> anyhow::Result<()> {
         match input.body.payload {
             Payload::Generate => {
-                let guid = Ulid::new().to_string();
+                let guid = format!("{}-{}", self.node, self.id);
                 let reply = Message {
                     src: input.dst,
                     dst: input.src,
@@ -46,31 +48,11 @@ impl Node<Payload> for UniqueNode {
                 self.id += 1;
             }
             Payload::GenerateOk { .. } => {}
-            Payload::Init { .. } => {
-                let reply = Message {
-                    src: input.dst,
-                    dst: input.src,
-                    body: Body {
-                        id: Some(self.id),
-                        in_reply_to: input.body.id,
-                        payload: Payload::InitOk,
-                    },
-                };
-                serde_json::to_writer(&mut *output, &reply)
-                    .context("serialize response to init")?;
-                output.write_all(b"\n").context("write trailing newline")?;
-                // reply
-                //     .serialize(output)
-                //     .context("Serialize response to init")
-                //     .unwrap();
-                self.id += 1;
-            }
-            Payload::InitOk { .. } => bail!("received init_ok message"),
         }
         Ok(())
     }
 }
 
 fn main() -> anyhow::Result<()> {
-    main_loop(UniqueNode { id: 0 })
+    main_loop::<_, UniqueNode, _>(())
 }
